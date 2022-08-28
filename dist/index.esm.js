6b3cc1d25a36e6b3da5104fc000643399a98683c
whernandez <37089069+whernandez@users.noreply.github.com>
2022-08-28 19:58:28 -0400

    first commit

---
+++ dist/index.esm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledMyComponent = styled.div `
+  color: ${p => p.color || 'pink'};
+`;
+const MyComponent = ({ text, color }) => (React.createElement(StyledMyComponent, { color: color }, text));
+
+export { MyComponent };
---
+++ playground/src/component-lib/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledMyComponent = styled.div `
+  color: ${p => p.color || 'pink'};
+`;
+const MyComponent = ({ text, color }) => (React.createElement(StyledMyComponent, { color: color }, text));
+
+export { MyComponent };
